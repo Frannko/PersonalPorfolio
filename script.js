a2a27c85ef3b4fad4d83ffbc323a97ec291ecfca
frannko <frannko13579@gmail.com>
2024-04-20 21:16:37 -0300

    Second steps

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var links = document.querySelectorAll("nav a");
+    var links = document.querySelectorAll(".contenedorheader a");
     links.forEach(function(link) {
     link.addEventListener("click", function(event) {
-        // Evitamos el comportamiento predeterminado del enlace
-        event.preventDefault();
         // Quitamos la clase 'selected' de todos los enlaces
         links.forEach(function(item) {
             item.classList.remove("selected");
